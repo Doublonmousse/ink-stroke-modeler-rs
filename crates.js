dd38eed9c1e1f13784519b6ec8cd01a8eb336512
Doublonmousse <Doublonmousse@users.noreply.github.com>
2024-10-30 19:14:20 +0000

    deploy: 84d311e9b0d034dcd955a1f353d37f54b2bda70f

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["ink_stroke_modeler_rs"];
+//{"start":21,"fragment_lengths":[23]}
---
+++ ink_stroke_modeler_rs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ModelerInputEventType"],"mod":["error"],"struct":["ModelerInput","ModelerParams","ModelerResult","StrokeModeler"]};
